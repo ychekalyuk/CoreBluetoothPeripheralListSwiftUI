b8b5ffd2d1542698541b60e731648917485696d1
Yurij Chekalyuk <yura_chekalyuk@mail.ru>
2023-02-11 00:38:38 +0300

    Show list of bluetooth devices

--- CoreBluetoothSwiftUI/ContentView.swift
+++ CoreBluetoothSwiftUI/ContentView.swift
@@ -6,14 +6,45 @@
 //
 
 import SwiftUI
+import CoreBluetooth
+
+class BluetoothViewModel: NSObject, ObservableObject {
+    private var centralManager: CBCentralManager?
+    private var peripherals: [CBPeripheral] = []
+    @Published var peripheralNames: [String] = []
+    
+    override init() {
+        super.init()
+        self.centralManager = CBCentralManager(delegate: self, queue: .main)
+    }
+}
+
+extension BluetoothViewModel: CBCentralManagerDelegate {
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        if central.state == .poweredOn {
+            self.centralManager?.scanForPeripherals(withServices: nil)
+        }
+    }
+    
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        if !peripherals.contains(peripheral) {
+            self.peripherals.append(peripheral)
+            self.peripheralNames.append(peripheral.name ?? "Unnamed Device")
+        }
+    }
+    
+    
+}
 
 struct ContentView: View {
+    @ObservedObject private var viewModel = BluetoothViewModel()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            List(viewModel.peripheralNames, id: \.self) { peripheral in
+                Text(peripheral)
+            }
+            .navigationTitle("Peripherals List")
         }
         .padding()
     }
