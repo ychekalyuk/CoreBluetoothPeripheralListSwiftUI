de37d7f88cac3dcb7fbb00d720b4e00fb6aa484e
Yurij Chekalyuk <yura_chekalyuk@mail.ru>
2023-02-10 23:42:10 +0300

    Initial Commit

---
+++ CoreBluetoothSwiftUI/CoreBluetoothSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreBluetoothSwiftUIApp.swift
+//  CoreBluetoothSwiftUI
+//
+//  Created by Юрий Альт on 10.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct CoreBluetoothSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
